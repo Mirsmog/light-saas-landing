75e668ca1dee31584e22076c13dd32055a2de547
zwine <tanharka.tyuchiev4@mail.ru>
2025-01-18 06:56:19 +0500

    feat: finish product-overview section

--- sections/product-overview/index.tsx
+++ sections/product-overview/index.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import ProductImage from '@/assets/product-image.png';
 import Image from 'next/image';
+import TubeImage from '@/assets/tube.png';
+import PyramidImage from '@/assets/pyramid.png';
+import ProductImage from '@/assets/product-image.png';
 
 export const ProductOverview = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <span className="tag">Boost your productivity</span>
-        <h2>A more effective way to track progress</h2>
-        <p>
-          Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
-        </p>
-        <Image
-          alt="our product overview image"
-          src={ProductImage.src}
-          width={ProductImage.width}
-          height={ProductImage.height}
-        />
+        <div className="mx-auto flex max-w-xl flex-col items-center gap-y-5 text-center">
+          <span className="tag">Boost your productivity</span>
+          <h2 className="bg-gradient-to-b from-black to-[#001E80] bg-clip-text text-3xl font-bold tracking-tighter text-transparent md:text-[54px] md:leading-[60px]">
+            A more effective way to track progress
+          </h2>
+          <p className="text-xl tracking-tight text-[#010D3E]">
+            Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
+          </p>
+        </div>
+        <div className="relative mt-10">
+          <Image
+            src={ProductImage.src}
+            width={ProductImage.width}
+            height={ProductImage.height}
+            alt="our product overview image"
+          />
+          <Image
+            className="absolute -right-36 -top-32 hidden h-auto max-w-[262px] md:block"
+            src={PyramidImage.src}
+            width={PyramidImage.width}
+            height={PyramidImage.height}
+            alt="pyramid"
+          />
+          <Image
+            className="absolute -left-36 bottom-14 hidden h-auto max-w-[248px] md:block lg:bottom-24"
+            src={TubeImage.src}
+            width={TubeImage.width}
+            height={TubeImage.height}
+            alt="pyramid"
+          />
+        </div>
       </div>
     </section>
   );
